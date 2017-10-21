0b219076239e1b5d6dc2a75267dd14e19f0b8e9a
root@win10 <root@win10>
2017-10-21 15:14:29 +0900

    .

--- 004-libcurl/app.d
+++ 004-libcurl/app.d
@@ -1,29 +1,47 @@
 // app.d
 
-private shared immutable ubyte[] libcurl_dll = cast(immutable ubyte[]) import(
-        "libcurl.dll@v2.076.1.bin");
+//private shared immutable ubyte[] libcurl_dll = cast(immutable ubyte[]) import(
+//        "libcurl.dll@v2.076.1.bin");
 //private shared ubyte[] libcurl_dll = cast(ubyte[]) import("libcurl.dll@v2.076.1.bin");
 
 void main(string[] args)
 {
-
+    prepare_libcurl();
     {
-        import std.file : thisExePath, read, write;
-        import std.path : dirName, dirSeparator;
+        import std.net.curl : byChunk;
         import std.stdio : stdout, writeln;
 
-        writeln(thisExePath());
-        string exe = thisExePath();
-        writeln("dirName(exe)=", dirName(exe));
-        writeln(dirName(exe) ~ dirSeparator ~ "temp.libcurl.dll");
-
-        writeln("libcurl_dll.length=", libcurl_dll.length);
-        write(dirName(exe) ~ dirSeparator ~ "temp.libcurl.dll", libcurl_dll);
-
-        ubyte[] bytes = cast(ubyte[])read(dirName(exe) ~ dirSeparator ~ "temp.libcurl.dll");
-        writeln("bytes.length=", bytes.length);
-        assert(bytes == libcurl_dll);
-
+        foreach (chunk; byChunk("dlang.org", 20))
+        {
+            writeln(chunk);
+            stdout.flush();
+        }
     }
+}
 
+private string prepare_libcurl()
+{
+    static shared immutable ubyte[] libcurl_dll = cast(immutable ubyte[]) import(
+            "libcurl.dll@v2.076.1.bin");
+    //static shared ubyte[] libcurl_dll = cast(ubyte[]) import("libcurl.dll@v2.076.1.bin"); /* writable */
+    import std.file : exists, read, thisExePath, write;
+    import std.path : dirName, dirSeparator;
+    import std.stdio : stdout, writeln;
+
+    immutable string exe = thisExePath();
+    writeln("exe=", exe);
+    immutable string out_path = dirName(exe) ~ dirSeparator ~ "libcurl.dll";
+    writeln("out_path=", out_path);
+    if (!exists(out_path))
+    {
+        writeln("libcurl_dll.length=", libcurl_dll.length);
+        write(out_path, libcurl_dll);
+        version (none)
+        {
+            ubyte[] bytes = cast(ubyte[]) read(out_path);
+            writeln("bytes.length=", bytes.length);
+            assert(bytes == libcurl_dll);
+        }
+    }
+    return out_path;
 }
