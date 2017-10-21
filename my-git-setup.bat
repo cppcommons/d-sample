3106995e2bc0b38a1639d59d37edfa915caea952
root <root@pc>
2017-10-21 18:32:53 +0900

    .

--- my-git-setup.bat
+++ my-git-setup.bat
@@ -4,8 +4,9 @@ git config --global user.email root@pc
 git config --unset-all credential.helper
 git config --global --unset-all credential.helper
 ::git config --system --unset-all credential.helper
+
 ::git config --global credential.helper wincred
 ::git config --global credential.helper store
 ::git config --global credential.helper cache
-git config credential.helper "cache --timeout=20"
+::git config credential.helper "cache --timeout=20"
 pause
