de6c46109ee5344d466b622be3233fa77815e671
root <root@pc>
2017-10-21 18:22:31 +0900

    .

---
+++ my-git-setup.bat
@@ -0,0 +1,6 @@
+git config --global user.name  root
+git config --global user.email root@pc
+::git config --global credential.helper wincred
+::git config --global credential.helper store
+git config --global credential.helper cache
+pause
