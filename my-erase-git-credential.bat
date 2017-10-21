214cad089c1d5c29593dab5ebcdae1b0b8fa69f5
root@win10 <root@win10>
2017-10-21 17:58:22 +0900

    Modified   my-erase-git-credential.bat

--- my-erase-git-credential.bat
+++ my-erase-git-credential.bat
@@ -1,3 +1,5 @@
 echo host=github.com
 echo protocol=https
 %ConEmuBaseDir%\..\..\..\vendor\git-for-windows\mingw32\libexec\git-core\git-credential-wincred.exe erase
+"C:\Program Files\Git\mingw64\libexec\git-core\git-credential-wincred.exe" erase
+pause
