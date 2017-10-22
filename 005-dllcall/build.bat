8e8bdbf54953b22455dc444152cb1daf43cf75f6
root <root@pc>
2017-10-23 08:32:30 +0900

    @

--- 005-dllcall/build.bat
+++ 005-dllcall/build.bat
@@ -4,5 +4,4 @@ del main.exe
 C:\D\dm\bin\dmc main.cpp lib_entry.cpp dll_data.c dll_data_1.c dll_data_2.c dll_data_3.c dll_data_4.c dll_data_5.c dll_data_6.c
 del lib_entry.lib
 C:\D\dm\bin\lib -c -p32 lib_entry.lib lib_entry.obj dll_data.obj dll_data_1.obj dll_data_2.obj dll_data_3.obj dll_data_4.obj dll_data_5.obj dll_data_6.obj
-
-::main.exe
+main.exe
--- 005-dllcall/main.cpp
+++ 005-dllcall/main.cpp
@@ -1,34 +1,14 @@
 #include <stdio.h>
 
-extern "C" void *get_proc(const char *proc_name);
-
+////extern "C" void *get_proc(const char *proc_name);
 extern "C" int add2(int a, int b);
-
 extern "C" int test1();
 
-#if 0x0
-extern int add2(int a, int b)
-{
-	typedef int (*proc_add2)(int a, int b);
-	static proc_add2 _add2 = (proc_add2)get_proc("add2");
-	return _add2(a, b);
-}
-
-extern int test1()
-{
-	typedef int (*proc_test1)();
-	static proc_test1 _test1 = (proc_test1)get_proc("test1");
-	return _test1();
-}
-#endif
 
 int main()
 {
-	//typedef int (*proc_test)();
-	//proc_test test1 = (proc_test)get_proc("test1");
 	int rc = test1();
 	printf("rc=%d\n", rc);
-
 	printf("add2(): %d\n", add2(111, 222));
 	return 0;
 }
