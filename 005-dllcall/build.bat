6524d43ede00d9f3bd72b62e0e58c2f46fd7789f
root <root@pc>
2017-10-23 08:29:50 +0900

    @

--- 005-dllcall/build.bat
+++ 005-dllcall/build.bat
@@ -2,4 +2,7 @@ qmake.exe dlltest.pro -spec win32-g++ "CONFIG+=release"
 mingw32-make -f Makefile.Release
 del main.exe
 C:\D\dm\bin\dmc main.cpp lib_entry.cpp dll_data.c dll_data_1.c dll_data_2.c dll_data_3.c dll_data_4.c dll_data_5.c dll_data_6.c
-main.exe
+del lib_entry.lib
+C:\D\dm\bin\lib -c -p32 lib_entry.lib lib_entry.obj dll_data.obj dll_data_1.obj dll_data_2.obj dll_data_3.obj dll_data_4.obj dll_data_5.obj dll_data_6.obj
+
+::main.exe
