eee5a19f69236bcb4142e414db91ee07e6e4e0df
root <root@pc>
2017-10-24 12:47:49 +0900

    @

--- 005-dllcall/app2.bat
+++ 005-dllcall/app2.bat
@@ -1,10 +1,10 @@
 setlocal
 del app2-dm32.exe
 dub build :app2 --build=release
-del *_data.c
-del *_data.obj
+del easy_win_*.c
+del easy_win_*.obj
 ::app2-dm32.exe my_library release/dlltest.dll 409600
-app2-dm32.exe my_library release/dlltest.dll 800000
+app2-dm32.exe my_library dlltest.dll 800000
 app2-dm32.exe sqlite sqlite-win-32bit-3200100.dll 800000
 app2-dm32.exe libcurl libcurl.dll 800000
 
