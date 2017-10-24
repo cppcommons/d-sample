2fe9f1772b3e48cadb2cba29fc02d6fa125fdfb8
root <root@pc>
2017-10-24 12:43:16 +0900

    @

--- 005-dllcall/app2.d
+++ 005-dllcall/app2.d
@@ -33,7 +33,7 @@ else
 		index++;
 	}
 
-	auto fname = format!"%s_0_codedata.c"(identifier);
+	auto fname = format!"easy_win_%s_0_codedata.c"(identifier);
 	File dll_data_h = File(fname, "w");
 	//dll_data_h.writef("extern \"C\" {\n");
 	for (int i=0; i<index; i++)
@@ -88,7 +88,7 @@ private void write_unit(string identifier, int index, ubyte[] bytes, ulong unit_
 	{
 		bytes ~= 0;
 	}
-	auto fname = format!"%s_%d_codedata.c"(identifier, index + 1);
+	auto fname = format!"easy_win_%s_%d_codedata.c"(identifier, index + 1);
 	auto f = File(fname, "w");
 	f.writef("extern const char easy_win_%s_%d[] = {\n", identifier, index + 1);
 	int first = true;
--- 005-dllcall/build.bat
+++ 005-dllcall/build.bat
@@ -9,16 +9,14 @@ del *_entry.obj
 
 setlocal enabledelayedexpansion enableextensions
 set LIST=
-for %%x in (my_library_*.c*) do set LIST=!LIST! %%x
-for %%x in (libcurl_*.c*) do set LIST=!LIST! %%x
-for %%x in (sqlite_*.c*) do set LIST=!LIST! %%x
+for %%x in (easy_win_*.c) do set LIST=!LIST! %%x
+for %%x in (entry_*.cpp) do set LIST=!LIST! %%x
 C:\D\dm\bin\dmc -c %LIST%
 
 setlocal enabledelayedexpansion enableextensions
 set LIST=
-for %%x in (my_library_*.obj) do set LIST=!LIST! %%x
-for %%x in (libcurl_*.obj) do set LIST=!LIST! %%x
-for %%x in (sqlite_*.obj) do set LIST=!LIST! %%x
+for %%x in (easy_win_*.obj) do set LIST=!LIST! %%x
+for %%x in (entry_*.obj) do set LIST=!LIST! %%x
 C:\D\dm\bin\lib -c -p512 lib_entry.lib %LIST%
 
 del main.exe
