40b1c7394302fce00d4fcfb7b2a024f3cbd43f01
root <root@pc>
2017-10-25 17:22:13 +0900

    @

--- 005-dllcall/vcapp1.cpp
+++ 005-dllcall/vcapp1.cpp
@@ -3,21 +3,25 @@
 #include <iostream>
 #include <memory>
 using namespace std;
-class C {
-private:
+class C
+{
+  private:
 	std::string name_;
 	int age_;
-public:
-	C(std::string name, int age) :name_(name), age_(age) {}
-	virtual ~C() {
-		trace(funcsig);
+
+  public:
+	C(std::string name, int age) : name_(name), age_(age) {}
+	virtual ~C()
+	{
+		trace(funcsig << name_ << ' '<< age_);
 	}
-	void doit() {
-		trace(funcsig);
+	void doit()
+	{
+		trace(funcsig << name_ << ' ' << age_);
 	}
 };
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
 	{
 		auto a = std::make_shared<C>("Foo", 123);
