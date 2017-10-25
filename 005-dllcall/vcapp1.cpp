40e6030769b38738b2e98fc17e8c142cb8f60e3c
root <root@pc>
2017-10-25 17:03:34 +0900

    @

--- 005-dllcall/vcapp1.cpp
+++ 005-dllcall/vcapp1.cpp
@@ -1,32 +1,30 @@
 //#ifdef ENABLE_TRACE
-#  ifdef _MSC_VER
-#    include <windows.h>
-#    include <sstream>
-#    define trace(x)                             \
-     do {  std::stringstream s;  s << x << '\n'; \
-           OutputDebugStringA(s.str().c_str());  \
-        } while(0)
-#  else
-#    include <iostream>
-#    define trace(x)  std::clog << (x)
-#  endif        // or std::cerr << (x) << std::flush
+#ifdef _MSC_VER
+#include <windows.h>
+#include <sstream>
+#define trace(x)                             \
+	do                                       \
+	{                                        \
+		std::stringstream s;                 \
+		s << x << '\n';                      \
+		OutputDebugStringA(s.str().c_str()); \
+	} while (0)
+#else
+#include <iostream>
+#define trace(x) std::clog << (x)
+#endif // or std::cerr << (x) << std::flush
 //#else
 //#  define trace(x)
 //#endif
 
-// vcapp1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
-
 //#include "stdafx.h"
 #include <stdio.h>
 
-
 int main()
 {
-	int     v1 = 123;
-	double  v2 = 456.789;
+	int v1 = 123;
+	double v2 = 456.789;
 	trace("main() v1=" << v1 << " v2=" << v2);
 	trace("main() v1=" << v1 << " v2=" << v2);
 	return 0;
 }
-
