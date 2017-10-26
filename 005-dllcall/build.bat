4aa7b9aa5cb6844d560e2ddd7b03ccf337e84cc7
Your Name <you@example.com>
2017-10-26 14:57:09 +0900

    @

--- 005-dllcall/build.bat
+++ 005-dllcall/build.bat
@@ -33,7 +33,9 @@ if %errorlevel% neq 0 ( exit /b )
 if "%1" neq "keep" (
   del easy_win_*
   del *.obj
-  del Makefile.*
+  del *.mk
+  del *.mk.Debug
+  del *.mk.Release
 )
 if "%1" equ "clean" (
   rmdir /s /q .dub
