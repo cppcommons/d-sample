dbaf94918855422a044b728d5c1a6d9ed5f35978
root <root@pc>
2017-10-27 20:48:57 +0900

    @

--- 005-dllcall/main.bat
+++ 005-dllcall/main.bat
@@ -7,10 +7,15 @@ for /f "delims=\ tokens=*" %%z in ("%SCRIPT%") do (
   set SCRIPT_CURRENT_DIR=%%~dpz
 )
 
-copy main.cmk CMakeLists.txt
-set FOLDER=main.bld
-
 cd /d %SCRIPT_CURRENT_DIR%
+copy main.cmk CMakeLists.txt
+
+set PATH=
+set PATH=%PATH%;C:\Program Files\CMake\bin
+set PATH=%PATH%;%SystemRoot%\System32
+
+set BOOST_ROOT=E:\boost_1_65_1
+set FOLDER=main.bld
 
 set REDO=0
 if "%1" equ "redo" set REDO=1
@@ -20,9 +25,7 @@ if "%REDO%"=="1" (
   rmdir /s /q %FOLDER%
   mkdir %FOLDER%
   cd %FOLDER%
-  cmake -G "Visual Studio 14 2015" -DCMAKE_CXX_FLAGS_RELEASE="/MT" -DCMAKE_CXX_FLAGS_DEBUG="/MTd" ^
-                                   -DBOOST_LIBRARYDIR=E:\boost_1_65_1\lib32-msvc-14.0 ^
-                                   ..
+  cmake -G "Visual Studio 14 2015" -DCMAKE_CXX_FLAGS_RELEASE="/MT" -DCMAKE_CXX_FLAGS_DEBUG="/MTd" ..
 )
 
 cd /d %SCRIPT_CURRENT_DIR%
