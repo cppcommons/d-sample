69981406c699b16b69efd80d1f3781e5dff23973
root <root@pc>
2017-10-28 02:31:37 +0900

    @

---
+++ 005-dllcall/a.d
@@ -0,0 +1,28 @@
+void main()
+{
+    import core.stdc.stdio;
+    import core.thread;
+    import std.stdio : writeln;
+
+    int i, j;
+    int times = 1000;
+    writeln("hello!");
+    setbuf(stdout, null);
+    fprintf(stdout, "stdout\n");
+    printf("0%%       50%%       100%%\n");
+    printf("+---------+---------+\n");
+    for (i = 0; i < times; i++)
+    {
+        /* 適当な処理 */
+        Thread.sleep(dur!("msecs")(5));
+        if (i % (times / 20) == times / 20 - 1)
+        {
+            /* プログレスバーの表示 */
+            for (j = 0; j < (i + 1) / (times / 20) + 1; j++)
+                printf("#");
+            /* キャリッジリターンを利用して先頭にカーソルを移動 */
+            printf("\r");
+        }
+    }
+    fprintf(stdout, "\n");
+}
