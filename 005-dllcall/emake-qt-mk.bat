a9d6ad0472e9066a618f56e69db34e01446d9419
root <root@pc>
2017-10-28 21:45:41 +0900

    @

--- 005-dllcall/emake-qt-mk.bat
+++ 005-dllcall/emake-qt-mk.bat
@@ -1,7 +1,11 @@
+setlocal
 start /w codeblocks --target=Release --build emake-qt.cbp
 if %errorlevel% neq 0 (
     echo Build Failed!
     exit /b )
 emake-qt.exe
-emake-qt.exe ___emake-gcc.pro emake.cpp common.h
-qmake -o ___emake-gcc.mk ___emake-gcc.pro
+set BASENAME=___emake-gcc
+emake-qt.exe %BASENAME%.pro emake.cpp common.h
+qmake -o %BASENAME%.mk %BASENAME%.pro
+mingw32-make -f %BASENAME%.mk.Release
+endlocal
