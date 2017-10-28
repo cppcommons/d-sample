575e6c6d9f9d47faf9c02a60195d61da58a22a75
root <root@pc>
2017-10-29 06:28:15 +0900

    @

--- 005-dllcall/edub-mk.bat
+++ 005-dllcall/edub-mk.bat
@@ -1,9 +1,11 @@
 setlocal
+emake-dmd edub.exe edub.d emake_common.d
 start /w codeblocks --target=Release --build edub.cbp
 if %errorlevel% neq 0 (
     echo Build Failed!
     start codeblocks edub.cbp
     exit /b )
-edub.exe
-edub.exe apps.json build :app1
+echo Build Successful!
+::edub.exe
+::edub.exe apps.json build :app1
 endlocal
