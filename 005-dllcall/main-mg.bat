ad253e96e494eebc341497403b70a085ef3dc022
root <root@pc>
2017-10-28 12:45:27 +0900

    @

--- 005-dllcall/main-mg.bat
+++ 005-dllcall/main-mg.bat
@@ -10,12 +10,8 @@ for /f "delims=\ tokens=*" %%z in ("%SCRIPT%") do (
 cd /d %SCRIPT_CURRENT_DIR%
 copy main.cmk CMakeLists.txt
 
-::set PATH=
-::set PATH=%PATH%;C:\Program Files\CMake\bin
-::set PATH=%PATH%;%SystemRoot%\System32
 set PATH=PATH=C:\Windows\system32;e:\opt\opt.m32\mingw32\qt5-static\bin;e:\opt\opt.m32\mingw32\bin;e:\opt\cmake-3.9.4-win64-x64\bin
 
-::set BOOST_ROOT=E:\boost_1_65_1
 set FOLDER=main-mg.bld
 
 set REDO=0
