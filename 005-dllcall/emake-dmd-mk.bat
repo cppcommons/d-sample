79c21889b2f7cdb5f72e07cbcaad81b1d3f0fa93
root <root@pc>
2017-10-29 05:56:16 +0900

    @

--- 005-dllcall/emake-dmd-mk.bat
+++ 005-dllcall/emake-dmd-mk.bat
@@ -1,10 +1,12 @@
 setlocal
+emake-dmd emake-dmd.exe emake-dmd.d emake_common.d
 start /w codeblocks --target=Release --build emake-dmd.cbp
 if %errorlevel% neq 0 (
     echo Build Failed!
     start codeblocks emake-dmd.cbp
     exit /b )
-emake-dmd.exe
-set BASENAME=emake-dmd-test___
-emake-dmd.exe %BASENAME%.exe emake-dmd.d emake_common.d
+echo Build Successful!
+::emake-dmd.exe
+::set BASENAME=emake-dmd-test___
+::emake-dmd.exe %BASENAME%.exe emake-dmd.d emake_common.d
 endlocal
