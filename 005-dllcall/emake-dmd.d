9071f8a3964c05aef3de64d9f6acff5539a8ba63
root <root@pc>
2017-10-29 10:52:20 +0900

    @

--- 005-dllcall/emake-dmd.d
+++ 005-dllcall/emake-dmd.d
@@ -12,123 +12,6 @@ import std.array : split;
 
 import emake_common;
 
-class EmakeCommand
-{
-    bool valid;
-    string compiler_type;
-    string[] args;
-
-    this(string compiler_type, string[] args)
-    {
-        this.compiler_type = compiler_type;
-        this.args = args;
-        this.parse_arguments();
-    }
-
-    ~this()
-    {
-    }
-
-    bool isValid()
-    {
-        return this.valid;
-    }
-
-    string[] command_type;
-    string project_file_name;
-    string project_file_ext;
-    string project_base_name;
-    string[] file_name_list;
-    string[] import_dir_list;
-    string[] lib_file_list;
-    string[] debug_arguments;
-    string exe_base_name;
-
-    bool check_command_type()
-    {
-        string header = this.args[0];
-        string[] header_parse = header.split("=");
-        switch (header_parse[0])
-        {
-        case "generate", "-":
-            header_parse[0] = "generate";
-            this.command_type = header_parse;
-            args = args[1 .. $];
-            break;
-        case "build":
-            this.command_type = header_parse;
-            args = args[1 .. $];
-            break;
-        case "run":
-            this.command_type = header_parse;
-            args = args[1 .. $];
-            break;
-        default:
-            this.command_type = ["generate", "release"];
-            break;
-        }
-        return true;
-    }
-
-    void parse_arguments()
-    {
-        this.valid = true;
-        string prog_name = args[0];
-        args = args[1 .. $];
-        if (args.length < 3)
-        {
-            writefln("Usage: %s PROJECT.exe source1 source2 ...", prog_name);
-            this.valid = false;
-            return;
-        }
-
-        if (!check_command_type())
-        {
-            this.valid = false;
-            return;
-        }
-
-        this.project_file_name = args[0];
-        args = args[1 .. $];
-
-        //writefln("project_file_name=%s", project_file_name);
-        this.project_file_ext = extension(this.project_file_name);
-        //writefln("project_file_ext=%s", project_file_ext);
-        if (this.project_file_ext != ".exe")
-        {
-            writefln("Project file name is invalid: %s", project_file_name);
-            this.valid = false;
-            return;
-        }
-        this.project_base_name = baseName(this.project_file_name, this.project_file_ext);
-        writefln("project_base_name=%s", this.project_base_name);
-        for (int i = 0; i < args.length; i++)
-        {
-            if (args[i] == "--")
-            {
-                debug_arguments = args[i + 1 .. $];
-                break;
-            }
-            // <Add directory="../../d-lib" />
-            if (args[i].startsWith("-I"))
-            {
-                //import_dir_list ~= args[i][2..$];
-                import_dir_list ~= args[i];
-                continue;
-            }
-            string file_name_ext = extension(args[i]);
-            if (file_name_ext == ".lib")
-            {
-                lib_file_list ~= args[i];
-                continue;
-            }
-            file_name_list ~= args[i];
-        }
-
-        this.exe_base_name = remove_surrounding_underscore(project_base_name);
-    }
-}
-
 private struct Target
 {
     string title;
@@ -202,7 +85,7 @@ private void put_build_target(ref Element elem, Target record)
 
 int main(string[] args)
 {
-    writeln(args.length);
+    ////writeln(args.length);
     auto emake_cmd = new EmakeCommand("dmd", args);
     if (!emake_cmd.isValid())
         return 1;
@@ -277,7 +160,7 @@ int main(string[] args)
     }
 
     // Pretty-print
-    writefln(join(doc.pretty(4), "\n"));
+    //writefln(join(doc.pretty(4), "\n"));
 
     file1.write(join(doc.pretty(4), "\n"));
     file1.close();
@@ -298,6 +181,5 @@ int main(string[] args)
     default:
         break;
     }
-
     return 0;
 }
--- 005-dllcall/emake_common.d
+++ 005-dllcall/emake_common.d
@@ -1,7 +1,9 @@
 module emake_common;
 import std.algorithm : startsWith, endsWith;
+import std.array : split;
+import std.path : baseName, extension;
 import std.process : pipeProcess, wait, Redirect;
-import std.stdio : writeln;
+import std.stdio;
 
 string remove_surrounding_underscore(string s)
 {
@@ -23,4 +25,121 @@ int emake_run_command(string[] dub_cmdline)
         writeln(line);
     int rc = wait(pipes.pid);
     return rc;
+}
+
+class EmakeCommand
+{
+    bool valid;
+    string compiler_type;
+    string[] args;
+
+    this(string compiler_type, string[] args)
+    {
+        this.compiler_type = compiler_type;
+        this.args = args;
+        this.parse_arguments();
+    }
+
+    ~this()
+    {
+    }
+
+    bool isValid()
+    {
+        return this.valid;
+    }
+
+    string[] command_type;
+    string project_file_name;
+    string project_file_ext;
+    string project_base_name;
+    string[] file_name_list;
+    string[] import_dir_list;
+    string[] lib_file_list;
+    string[] debug_arguments;
+    string exe_base_name;
+
+    bool check_command_type()
+    {
+        string header = this.args[0];
+        string[] header_parse = header.split("=");
+        switch (header_parse[0])
+        {
+        case "generate", "-":
+            header_parse[0] = "generate";
+            this.command_type = header_parse;
+            args = args[1 .. $];
+            break;
+        case "build":
+            this.command_type = header_parse;
+            args = args[1 .. $];
+            break;
+        case "run":
+            this.command_type = header_parse;
+            args = args[1 .. $];
+            break;
+        default:
+            this.command_type = ["generate", "release"];
+            break;
+        }
+        return true;
+    }
+
+    void parse_arguments()
+    {
+        this.valid = true;
+        string prog_name = args[0];
+        args = args[1 .. $];
+        if (args.length < 3)
+        {
+            writefln("Usage: %s PROJECT.exe source1 source2 ...", prog_name);
+            this.valid = false;
+            return;
+        }
+
+        if (!check_command_type())
+        {
+            this.valid = false;
+            return;
+        }
+
+        this.project_file_name = args[0];
+        args = args[1 .. $];
+
+        //writefln("project_file_name=%s", project_file_name);
+        this.project_file_ext = extension(this.project_file_name);
+        //writefln("project_file_ext=%s", project_file_ext);
+        if (this.project_file_ext != ".exe")
+        {
+            writefln("Project file name is invalid: %s", project_file_name);
+            this.valid = false;
+            return;
+        }
+        this.project_base_name = baseName(this.project_file_name, this.project_file_ext);
+        writefln("project_base_name=%s", this.project_base_name);
+        for (int i = 0; i < args.length; i++)
+        {
+            if (args[i] == "--")
+            {
+                debug_arguments = args[i + 1 .. $];
+                break;
+            }
+            // <Add directory="../../d-lib" />
+            if (args[i].startsWith("-I"))
+            {
+                //import_dir_list ~= args[i][2..$];
+                import_dir_list ~= args[i];
+                continue;
+            }
+            string file_name_ext = extension(args[i]);
+            if (file_name_ext == ".lib")
+            {
+                lib_file_list ~= args[i];
+                continue;
+            }
+            file_name_list ~= args[i];
+        }
+
+        this.exe_base_name = remove_surrounding_underscore(project_base_name);
+    }
 }
