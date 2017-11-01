0436a2710f14c592682bf5dda849b6b7eb2219c8
root <root@pc>
2017-11-01 10:09:35 +0900

    @

--- 005-dllcall/emake-dmd-mk.bat
+++ 005-dllcall/emake-dmd-mk.bat
@@ -2,7 +2,8 @@
 ::dmd -ofemake-dmd.exe -g -debug -o- emake-dmd.d emake_common.d
 del emake-dmd.exe
 rmdir /s /q emake-dmd.exe.bin
-dmd -of=emake-dmd.exe -od=emake-dmd.exe.bin emake-dmd.d emake_common.d emake_common_codeblocks.d
+::dmd -of=emake-dmd.exe -od=emake-dmd.exe.bin emake-dmd.d emake_common.d emake_common_codeblocks.d
+dmd -of=emake-dmd.exe -od=emake-dmd.exe.bin -g -debug emake-dmd.d emake_common.d emake_common_codeblocks.d
 if %errorlevel% neq 0 ( exit /b )
 copy emake-dmd.exe E:\opt\bin32\
 ::emake-dmd edit x.exe a.d -IC:\test\import
