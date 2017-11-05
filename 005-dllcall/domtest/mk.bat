5b8d78e86f450090aca3b3b30eb4b3dd836081c5
root <root@pc>
2017-11-05 16:46:43 +0900

    @

--- 005-dllcall/domtest/mk.bat
+++ 005-dllcall/domtest/mk.bat
@@ -1,14 +1,16 @@
 edub domtest.exe build src=. ^
 app.d ^
-arsd/characterencodings.d@https://github.com/adamdruppe/arsd/blob/master/characterencodings.d ^
-arsd/curl.d@https://github.com/adamdruppe/arsd/blob/master/curl.d ^
-arsd/dom.d@https://github.com/adamdruppe/arsd/blob/master/dom.d ^
+characterencodings.d@https://github.com/adamdruppe/arsd/blob/master/characterencodings.d ^
+curl.d@https://github.com/adamdruppe/arsd/blob/master/curl.d ^
+dom.d@https://github.com/adamdruppe/arsd/blob/master/dom.d ^
 ../lib_entry.lib
 echo errorlevel=%errorlevel%
 
+goto skip
 edub test1.exe init src=. ^
 app.d ^
 arsd/characterencodings.d@https://github.com/adamdruppe/arsd/blob/master/characterencodings.d ^
 arsd/curl.d@https://github.com/adamdruppe/arsd/blob/master/curl.d ^
 arsd/dom.d@https://github.com/adamdruppe/arsd/blob/master/dom.d ^
 [d2sqlite3:~master:all-included]
+:skip
