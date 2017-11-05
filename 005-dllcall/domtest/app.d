9622b468463f44ac9a5bf89c2f2f21afe433df9e
root <root@pc>
2017-11-05 22:34:42 +0900

    @

--- 005-dllcall/domtest/app.d
+++ 005-dllcall/domtest/app.d
@@ -1,12 +1,15 @@
 import arsd.dom;
 import easy.windows.std.net.curl;
+import std.array;
+import std.conv;
 import std.json;
 import std.stdio;
+import std.string;
 
 void main()
 {
 	//string html = curl("https://qiita.com/search?sort=created&q=created%3A2016-12");
-	string html = cast(string)get("https://qiita.com/search?sort=created&q=created%3A2016-12");
+	string html = cast(string) get("https://qiita.com/search?sort=created&q=created%3A2016-12");
 	writeln(html);
 	auto document = new Document();
 	document.parseGarbage(html);
@@ -14,26 +17,26 @@ void main()
 	Element[] elems = document.getElementsByClassName(`searchResult`);
 	writeln(elems.length);
 	JSONValue array = parseJSON(`[]`);
-	foreach(ref elem; elems)
+	foreach (ref elem; elems)
 	{
 		//writeln(elem.outerHTML);
 		JSONValue rec = parseJSON(`{}`);
 		rec.object["data-uuid"] = elem.getAttribute(`data-uuid`);
-		writeln();
-		writeln(`data-uuid=`, elem.getAttribute(`data-uuid`));
-		writeln(elem.getElementsByClassName(`searchResult_statusList`)[0].innerText);
-		writeln(elem.getElementsByClassName(`searchResult_itemTitle`)[0].innerText);
-		Element a = elem.getElementsByClassName(`searchResult_itemTitle`)[0].requireSelector("a");
-		writeln(a.getAttribute("href"));
-		writeln(elem.getElementsByClassName(`searchResult_header`)[0].innerHTML);
-		writeln(elem.getElementsByClassName(`searchResult_header`)[0].innerText);
-		writeln(elem.getElementsByClassName(`tagList`)[0].innerHTML);
-		writeln(elem.getElementsByClassName(`tagList`)[0].innerText);
-		writeln(elem.getElementsByClassName(`searchResult_snippet`)[0].innerText);
-		foreach(ref tag; elem.getElementsByClassName(`tagList_item`))
+		rec.object["fav-count"] = to!long(
+				elem.getElementsByClassName(`searchResult_statusList`)[0].innerText.strip);
+		rec.object["title"] = elem.getElementsByClassName(`searchResult_itemTitle`)[0].innerText;
+		rec.object["href"] = elem.getElementsByClassName(
+				`searchResult_itemTitle`)[0].requireSelector("a").getAttribute("href");
+		rec.object["header"] = elem.getElementsByClassName(`searchResult_header`)[0].innerText;
+		rec.object["description"] = elem.getElementsByClassName(
+				`searchResult_snippet`)[0].innerText;
+		string[] tag_array;
+		foreach (ref tag; elem.getElementsByClassName(`tagList_item`))
 		{
 			writefln("tag=%s", tag.innerText);
+			tag_array ~= tag.innerText;
 		}
+		rec.object["tags"] = tag_array.join(`|`);
 		array.array ~= rec;
 		writeln();
 	}
