e4d780691e34716ab176a11c9a41e7983af41172
Your Name <you@example.com>
2017-11-06 12:49:54 +0900

    .

--- 005-dllcall/edub-mk.bat
+++ 005-dllcall/edub-mk.bat
@@ -4,5 +4,6 @@ rmdir /s /q edub.exe.bin
 dmd -of=edub.exe -od=edub.exe.bin -g -debug edub.d
 if %errorlevel% neq 0 ( exit /b )
 copy edub.exe E:\opt\bin32\
+copy edub.exe C:\Users\Public\dev1\bin32
 rmdir /s /q edub.exe.bin
 del edub.exe
