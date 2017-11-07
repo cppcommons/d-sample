dacbdcc901b197ecda2dd21d1feee9d954f4d8d8
root <root@pc>
2017-11-07 19:18:46 +0900

    @

--- 005-dllcall/domtest/control.d
+++ 005-dllcall/domtest/control.d
@@ -1,13 +1,18 @@
 import dateparser;
-import std.net.curl;
 import jsonizer;
+
+//import core.sync.barrier;
+import core.sync.rwmutex;
+import core.thread;
 import std.array;
 import std.conv;
 import std.datetime;
 import std.file;
 import std.format;
 import std.json;
+import std.net.curl;
 import std.path;
+import std.process;
 import std.regex;
 import std.stdio;
 import std.string;
@@ -35,6 +40,15 @@ struct QPost
 	}
 }
 
+static __gshared SysTime[] schedule;
+
+//__gshared ReadWriteMutex mutex;
+private __gshared ReadWriteMutex mutex;
+static this()
+{
+	mutex = new ReadWriteMutex(ReadWriteMutex.Policy.PREFER_WRITERS);
+}
+
 version (TEST1) int main(string[] args)
 {
 	// "2011/09/16"
@@ -45,7 +59,7 @@ version (TEST1) int main(string[] args)
 	}
 	else
 	{
-		const short year0 = 2016;
+		const short year0 = 2017;
 		const ubyte month0 = 9;
 	}
 	SysTime v_date = Clock.currTime();
@@ -59,9 +73,70 @@ version (TEST1) int main(string[] args)
 			if (year == year0 && month < month0)
 				continue;
 			writefln(`year=%d month=%u`, year, month);
+			schedule ~= SysTime(DateTime(year, month, 1));
 			if (year == v_date.year && month == v_date.month)
 				break a;
 		}
 	}
+	foreach (v_sample; schedule)
+	{
+		writeln(v_sample);
+	}
+	synchronized (mutex.writer)
+	{
+		writeln("writing...");
+	}
+	synchronized (mutex.reader)
+	{
+		writeln("reading...");
+	}
+
+	bool pop(out SysTime head)
+	{
+		if (schedule.length == 0)
+			return false;
+		head = schedule[0];
+		schedule = schedule[1 .. $];
+		return true;
+	}
+
+	int run_command(string[] cmdline)
+	{
+		auto pipes = pipeProcess(cmdline, Redirect.stdout | Redirect.stderrToStdout);
+		foreach (line; pipes.stdout.byLine)
+		{
+			import std.stdio : stdout;
+
+			writeln(line);
+			stdout.flush();
+		}
+		int rc = wait(pipes.pid);
+		return rc;
+	}
+
+	void writerFn()
+	{
+		SysTime v_st;
+		synchronized (mutex.writer)
+		{
+			if (!pop(v_st))
+				return;
+		}
+		writeln(v_st);
+		string v_period = format!`%04d-%02d`(v_st.year, v_st.month);
+		writeln(v_period);
+		string[] cmd = ["domtest.exe", v_period];
+		run_command(cmd);
+	}
+
+	Thread t1 = new Thread(&writerFn).start();
+	Thread t2 = new Thread(&writerFn).start();
+	//Thread t3 = new Thread(&writerFn).start();
+	while (t1.isRunning || t2.isRunning)
+	{
+		Thread.sleep(dur!("msecs")(50));
+	}
+
+	writeln("All finished!");
 	return 0;
 }
