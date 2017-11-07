d4f7924ca5acaf072eacc7ae707e515539b112de
root <root@pc>
2017-11-08 04:30:53 +0900

    @

--- 005-dllcall/domtest/qiita.d
+++ 005-dllcall/domtest/qiita.d
@@ -8,6 +8,7 @@ import core.thread;
 import std.array;
 import std.conv;
 import std.datetime;
+import std.datetime.systime;
 import std.file;
 import std.format;
 import std.json;
@@ -30,8 +31,13 @@ int main(string[] args)
 	// Get with custom data receivers
 	auto http = HTTP("http://qiita.com/api/v2/items/1a182f187fd2a8df29c2");
 	http.addRequestHeader(`Authorization`, `Bearer 06ade23e3803334f43a0671f2a7c5087305578bd`);
+	long v_rate_reset = 0;
 	http.onReceiveHeader = (in char[] key, in char[] value) {
 		writeln(key ~ ": " ~ value);
+		if (key == `rate-reset`)
+		{
+			v_rate_reset = to!long(value);
+		}
 	};
 	ubyte[] bytes;
 	http.onReceive = (ubyte[] data) { /+ drop +/
@@ -44,10 +50,22 @@ int main(string[] args)
 	//writeln(cast(char[]) bytes);
 	JSONValue jsonObj = parseJSON(cast(char[]) bytes);
 	assert(jsonObj.type == JSON_TYPE.OBJECT);
-	foreach(key; jsonObj.object.keys)
+	foreach (key; jsonObj.object.keys)
 	{
 		writeln(key);
 	}
 	writeln(jsonObj.object[`created_at`].toString);
+	writeln(v_rate_reset);
+	writeln(SysTime(unixTimeToStdTime(v_rate_reset)));
+	SysTime currentTime = Clock.currTime();
+	writeln(currentTime);
+	SysTime v_reset_time = SysTime(unixTimeToStdTime(v_rate_reset));
+	//auto diff = v_reset_time - currentTime;
+	Duration diff = v_reset_time - currentTime;
+	writeln(diff);
+	writeln(diff.total!"minutes");
+	writeln(diff.total!"seconds");
+	writeln(diff.total!"msecs");
+
 	return 0;
 }
