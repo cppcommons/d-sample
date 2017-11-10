666fe6d5eadc21842ac0e7f43d8d000e17383b92
root <root@pc>
2017-11-10 16:03:10 +0900

    @

--- 005-dllcall/idl-mk.bat
+++ 005-dllcall/idl-mk.bat
@@ -1,4 +1,5 @@
-edub idl.exe build inc=../../d-lib idl.d ../../d-lib/pegged-dm32.lib --build=release --cleanup
+::edub idl.exe build inc=../../d-lib idl.d ../../d-lib/pegged-dm32.lib --build=release --cleanup
+edub idl.exe build inc=../../d-lib idl.d ../../d-lib/pegged-dm32.lib --build=release
 if %errorlevel% neq 0 ( exit /b )
 chcp 65001
 idl.exe idl-test.txt
