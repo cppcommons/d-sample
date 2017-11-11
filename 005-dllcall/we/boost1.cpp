4996a4dee4490eb0fc16b6d97c662e8a1f8ebca6
root <root@pc>
2017-11-11 16:26:14 +0900

    @

--- 005-dllcall/we/boost1.cpp
+++ 005-dllcall/we/boost1.cpp
@@ -15,6 +15,8 @@ using namespace std;
 #define _MT
 #include <process.h>
 
+#define DBG(format, ...) os_printf("[DEBUG] " format "\n", ##__VA_ARGS__)
+
 //stlsoft::winstl_project::thread_mutex g_thread_mutex;
 
 struct os_thread_locker
@@ -29,6 +31,57 @@ struct os_thread_locker
 		m_mutex.unlock();
 	}
 };
+
+static int os_write_consoleA(HANDLE hconsole, const char *format, va_list args)
+{
+	char v_buffer[1024 + 1];
+	v_buffer[1024] = 0;
+	int len = wvsprintfA((LPSTR)v_buffer, format, args); // Win32 API
+	for (int i = 0; i < len; i++)
+	{
+		if (v_buffer[i] == 0)
+		{
+			v_buffer[i] = '@';
+		}
+	}
+	DWORD dwWriteByte;
+	WriteConsoleA(hconsole, v_buffer, len, &dwWriteByte, NULL);
+	OutputDebugStringA((LPCSTR)v_buffer);
+	return len;
+}
+
+int os_printf(const char *format, ...)
+{
+	static stlsoft::winstl_project::thread_mutex v_mutex;
+	{
+		os_thread_locker locker(v_mutex);
+		va_list args;
+		va_start(args, format);
+		int len = os_write_consoleA(GetStdHandle(STD_OUTPUT_HANDLE), format, args);
+		va_end(args);
+		return len;
+	}
+}
+
+int os_dbg(const char *format, ...)
+{
+	//cout << "os_dbg(1)" << endl;
+	static stlsoft::winstl_project::thread_mutex v_mutex;
+	{
+		os_thread_locker locker(v_mutex);
+		//cout << "os_dbg(2)" << endl;
+		char v_buffer[1024 + 1];
+		v_buffer[1024] = 0;
+		wsprintfA((LPSTR)v_buffer, "[DEBUG] %s\n", format); // Win32 API
+		//cout << "os_dbg(3)" << endl;
+		va_list args;
+		va_start(args, format);
+		int len = os_write_consoleA(GetStdHandle(STD_OUTPUT_HANDLE), v_buffer, args);
+		va_end(args);
+		//cout << "os_dbg(4)" << endl;
+		return len;
+	}
+}
 
 ::int64_t os_get_thread_id()
 {
@@ -50,9 +103,9 @@ struct os_object_entry_t
 {
 	::int64_t m_link_count;
 	::int64_t m_value;
-	explicit os_object_entry_t(): m_link_count(0), m_value(0)
+	explicit os_object_entry_t() : m_link_count(0), m_value(0)
 	{
-		cout << R"(\m_link_count\=)" << m_link_count << endl;
+		os_dbg(R"(\m_link_count\=%ld)", m_link_count);
 	}
 };
 
@@ -63,6 +116,7 @@ typedef std::map<std::string, void *> func_map_t;
 typedef stlsoft::shared_ptr<func_map_t> func_map_ptr_t;
 typedef stlsoft::shared_ptr<func_map_t> func_map_ptr_t2;
 
+#if 0x0
 struct cos_state
 {
 	//void *func_map;
@@ -71,12 +125,12 @@ struct cos_state
 	//func_map_t func_map;
 	explicit cos_state() : func_map(nullptr)
 	{
-		cout << "cos_state()" << endl;
+		os_dbg("cos_state()");
 		func_map = new func_map_t;
 	}
 	/*virtual*/ ~cos_state()
 	{
-		cout << "~cos_state()" << endl;
+		os_dbg("~cos_state()");
 		delete func_map;
 	}
 };
@@ -86,6 +140,7 @@ typedef stlsoft::shared_ptr<cos_state> cos_state_ptr;
 //static TLS_VARIABLE_DECL cos_state_ptr g_state;
 //static TLS_VARIABLE_DECL cos_state g_state;
 //static cos_state g_state;
+#endif
 
 struct MYHANDLE
 {
@@ -113,11 +168,11 @@ struct C_Class1
 {
 	explicit C_Class1()
 	{
-		cout << "Constructor" << endl;
+		os_dbg("Constructor");
 	}
 	virtual ~C_Class1()
 	{
-		cout << "Destructor" << endl;
+		os_dbg("Destructor");
 	}
 	void Release()
 	{
@@ -171,14 +226,14 @@ unsigned __stdcall sure1(void *p)
 DWORD WINAPI Thread(LPVOID *data)
 {
 	DWORD wintid = GetCurrentThreadId();
-	cout << "wintid=" << wintid << endl;
+	os_dbg("wintid=%u", wintid);
 	::int64_t tid1 = os_get_thread_id();
-	cout << tid1 << endl;
-	printf("%s start\n", (const char *)data);
+	os_dbg("%ld", tid1);
+	os_dbg("%s start", (const char *)data);
 	Sleep(1000);
 	::int64_t tid2 = os_get_thread_id();
-	cout << tid2 << endl;
-	printf("%s end\n", (const char *)data);
+	os_dbg("%ld", tid2);
+	os_dbg("%s end", (const char *)data);
 	ExitThread(0);
 	return 0;
 }
@@ -186,7 +241,7 @@ DWORD WINAPI Thread(LPVOID *data)
 int main()
 {
 	::int64_t tid = os_get_thread_id();
-	cout << tid << endl;
+	os_dbg("%ld", tid);
 	HANDLE hThread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Thread, (LPVOID) "カウント数表示：", 0, NULL);
 
 #if 0x0
@@ -207,8 +262,8 @@ int main()
 	v1.push_back(StrPtr(new string("a")));
 	v1.push_back(s);
 
-	cout << *s << endl;						 // sをpush_backで他にコピーしたからと言って使えなくなったりしない
-	cout << "*s.get()=" << *s.get() << endl; // sをpush_backで他にコピーしたからと言って使えなくなったりしない
+	os_dbg("%s", (*s).c_str());
+	os_dbg("%s", (*s.get()).c_str());
 #endif
 	typedef stlsoft::shared_ptr<C_Class1> ClsPtr;
 	ClsPtr c1 = ClsPtr(new C_Class1());
@@ -216,7 +271,7 @@ int main()
 	/*ClsPtr c3 =*/ClsPtr(new C_Class1());
 	ClsPtr c4 = c1;
 
-	cout << "c1.use_count()=" << c1.use_count() << endl;
+	os_dbg("c1.use_count()=%d", c1.use_count());
 
 	C_Variant var1 = 123;
 	C_Variant var2 = "abc";
