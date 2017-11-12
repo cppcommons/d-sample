0960bd2aa2d9f7335e9455148353fa57920a17ba
root <root@pc>
2017-11-12 23:52:28 +0900

    @

--- 005-dllcall/we/a.bat
+++ 005-dllcall/we/a.bat
@@ -1,3 +1,3 @@
 chcp 65001
-edub a-run.exe run a.d --build=release os.lib lib1.lib c:\dm\lib\stlp45dm_static.lib
+edub a-run.exe run a.d --build=release os.lib lib1.lib c:\dm\lib\stlp45dm_static.lib wininet.lib
 if %errorlevel% neq 0 ( exit /b )
--- 005-dllcall/we/a.d
+++ 005-dllcall/we/a.d
@@ -1,6 +1,7 @@
 import os;
 import lib1;
 
+import core.sys.windows.windows;
 import core.sys.windows.wininet;
 
 private void exit(int code)
@@ -31,6 +32,45 @@ void main(string[] args)
 	//import core.stdc.stdio;
 	import core.thread;
 	import std.stdio;
+	import std.string;
+
+	string app = "Sample Application/1.0";
+	string url = "https://raw.githubusercontent.com/cyginst/ms2inst-v1/master/ms2inst.bat";
+
+	char* appZ = cast(char*) toStringz(app);
+
+	HINTERNET hInet;
+	HINTERNET hFile;
+	//char *lpszBuf;
+	char[1024] lpszBuf;
+	DWORD dwSize;
+
+	//lpszBuf = (char *)GlobalAlloc(GPTR, 1024);
+
+	/* ハンドル作成 */
+	hInet = InternetOpenA("TEST", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+
+	/* URLオープン */
+	hFile = InternetOpenUrlA(hInet,//"http://www.sm.rim.or.jp/~shishido/src/httpt.txt",
+			"https://raw.githubusercontent.com/cyginst/cyginst-v1/master/cyginst.bat",
+			NULL, 0, INTERNET_FLAG_RELOAD, 0);
+
+	char[] result;
+	while (InternetReadFile(hFile, cast(char *)lpszBuf.ptr, 1023, &dwSize) && dwSize > 0)
+	{
+		//printf("%s", lpszBuf);
+		//result.append(lpszBuf, dwSize);
+		result ~= lpszBuf[0..dwSize];
+	}
+	//printf("\n");
+	//printf("%s\n", result.c_str());
+	writeln(result);
+
+	/* 終了処理 */
+	InternetCloseHandle(hFile);
+	InternetCloseHandle(hInet);
+
+	exit(0);
 
 	os_value[] argv;
 	argv ~= os_new_integer(11);
@@ -49,7 +89,7 @@ void main(string[] args)
 	os_link(mystr);
 	os_sweep();
 	os_dump_heap();
-	char *ptr = os_get_string(mystr);
+	char* ptr = os_get_string(mystr);
 	writefln("[%s]", toString(ptr));
 
 	exit(0);
