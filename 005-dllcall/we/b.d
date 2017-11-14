4dd9bb684b77c59b268649f5afeba10fb48c0b5e
root <root@pc>
2017-11-14 11:52:24 +0900

    @

--- 005-dllcall/we/b.d
+++ 005-dllcall/we/b.d
@@ -1,9 +1,43 @@
+import os1;
+
+extern (C)
+{
+	alias ulong os_value;
+	alias os_value function(int argc, os_value* argv) os_function;
+	enum os_type
+	{
+		OS_NIL,
+		OS_ARRAY,
+		OS_BYTES,
+		OS1_HANDLE,
+		OS_INTEGER,
+		OS_REAL,
+		OS_STRING,
+	}
+
+	long os_get_length(os_value value);
+	os_value os_new_array(long len);
+	os_value* os_get_array(os_value value);
+	os_value os_new_handle(void* data);
+	void* os_get_handle(os_value value);
+	os_value os_new_integer(long data);
+	long os_get_integer(os_value value);
+	os_value os_new_string(char* data, long len);
+	char* os_get_string(os_value value);
+	void os_dump_heap();
+	bool os_mark(os_value entry);
+	bool os_unmark(os_value entry);
+	void os_sweep();
+	void os_clear();
+}
+
 import core.sync.mutex;
 import std.algorithm.sorting;
 import std.array;
 import std.format;
 import std.stdio;
 
+/+
 alias ulong os_value;
 alias os_value function(int argc, os_value* argv) os_function;
 enum os_type
@@ -16,7 +50,7 @@ enum os_type
 	OS_REAL,
 	OS_STRING,
 }
-
++/
 static __gshared Mutex g_os_thread_mutex;
 static this()
 {
