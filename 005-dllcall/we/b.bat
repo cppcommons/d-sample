521d8884f2afcc4094b60db47a516daf75a6cfc1
root <root@pc>
2017-11-16 08:46:14 +0900

    @

--- 005-dllcall/we/b.bat
+++ 005-dllcall/we/b.bat
@@ -1,4 +1,46 @@
-chcp 65001
-edub b-run.exe run b.d lib1.d lib2.d --build=release lib1.lib c:\dm\lib\stlp45dm_static.lib wininet.lib
-::edub b-run.exe run b.d --build=debug lib1.lib c:\dm\lib\stlp45dm_static.lib wininet.lib
-if %errorlevel% neq 0 ( exit /b )
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
+@echo on
+cl /MD minimal_client.c ^
+ -IE:\opt\svn\vc6\svn-win32-1.7.22_dev\svn-win32-1.7.22\include ^
+ -IE:\opt\svn\vc6\svn-win32-1.7.22_dev\svn-win32-1.7.22\include\apr ^
+ -IE:\opt\svn\vc6\svn-win32-1.7.22_dev\svn-win32-1.7.22\include\apr-iconv ^
+ -IE:\opt\svn\vc6\svn-win32-1.7.22_dev\svn-win32-1.7.22\include\apr-util ^
+ /link /out:mini.exe ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib/apr ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib/apr-iconv ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib/apr-util ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib/neon ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib/sasl ^
+  /LIBPATH:E:/opt/svn/vc6/svn-win32-1.7.22_dev/svn-win32-1.7.22/lib/serf ^
+		"libapr-1.lib" ^
+		"libapriconv-1.lib" ^
+		"libaprutil-1.lib" ^
+		"xml.lib" ^
+		"libsvn_client-1.lib" ^
+		"libsvn_delta-1.lib" ^
+		"libsvn_diff-1.lib" ^
+		"libsvn_fs-1.lib" ^
+		"libsvn_fs_base-1.lib" ^
+		"libsvn_fs_fs-1.lib" ^
+		"libsvn_fs_util-1.lib" ^
+		"libsvn_ra-1.lib" ^
+		"libsvn_ra_local-1.lib" ^
+		"libsvn_ra_serf-1.lib" ^
+		"libsvn_ra_svn-1.lib" ^
+		"libsvn_repos-1.lib" ^
+		"libsvn_subr-1.lib" ^
+		"libsvn_wc-1.lib" ^
+		"libsasl.lib" ^
+		"serf-1.lib" ^
+		"svn_client-1.lib" ^
+		"svn_delta-1.lib" ^
+		"svn_diff-1.lib" ^
+		"svn_fs-1.lib" ^
+		"svn_ra-1.lib" ^
+		"svn_repos-1.lib" ^
+		"svn_subr-1.lib" ^
+		"svn_wc-1.lib"
+endlocal
+copy mini.exe E:\opt\svn\vc6\svn-win32-1.7.22\bin
--- 005-dllcall/we/vc6.bat
+++ 005-dllcall/we/vc6.bat
@@ -1,5 +1,15 @@
 premake --file vc6.pmk --target vs6
-::"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.com" vc6-pkg.dsp /MAKE ALL /REBUILD
-"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.com" vc6-run.dsp /MAKE ALL /BUILD
+"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.com" vc6-run.dsp /MAKE ALL /REBUILD
+::"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.com" vc6-run.dsp /MAKE ALL /BUILD
 ::premake --file vc6.pmk --clean
-vc6-run.exe
+setlocal
+::set PATH=E:\opt\svn\vc6\svn-win32-1.8.17-ap24\svn-win32-1.8.17\bin;%PATH%
+::vc6-run.exe https://github.com/cppcommons/d-sample/trunk
+copy vc6-run.exe E:\opt\svn\vc6\svn-win32-1.7.22\bin
+E:\opt\svn\vc6\svn-win32-1.7.22\bin\vc6-run.exe https://github.com/cppcommons/d-sample/trunk
+::copy vc6-run.exe E:\opt\svn\vc6\svn-win32-1.8.17-ap24\svn-win32-1.8.17\bin
+::E:\opt\svn\vc6\svn-win32-1.8.17-ap24\svn-win32-1.8.17\bin\vc6-run.exe https://github.com/cppcommons/d-sample/trunk
+::copy vc6-run.exe E:\opt\svn\vc6\svn-win32-1.6.19\bin
+::E:\opt\svn\vc6\svn-win32-1.6.19\bin\vc6-run.exe https://github.com/cppcommons/d-sample/trunk
+::E:\opt\svn\vc6\svn-win32-1.8.17-ap24\svn-win32-1.8.17\bin\svn.exe ls https://github.com/cppcommons/d-sample/trunk
+endlocal
