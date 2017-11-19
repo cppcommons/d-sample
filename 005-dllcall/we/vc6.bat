8f7129fc89b6ccf9f7e770fe617f57d2b8c9f8cf
root <root@super-computer>
2017-11-19 18:05:24 +0900

    @

--- 005-dllcall/we/vc6.bat
+++ 005-dllcall/we/vc6.bat
@@ -4,18 +4,12 @@ pexports e:\opt\opt.m32\usr\bin\msys-apr-1-0.dll > msys-apr-1-0.def
 lib /DEF:msys-apr-1-0.def /MACHINE:X86 /OUT:msys-apr-1-0.lib
 if %errorlevel% neq 0 ( exit /b )
 
-::cp vc6.cpp vc6_iface.h
-::c:\dm\bin\htod -cpp -hc vc6_iface.h
-::cat vc6_iface.d
 c:\dm\bin\htod -cpp -hc vc6.cpp
 if %errorlevel% neq 0 ( exit /b )
-::premake --file vc6-dll.pmk --target vs6
-::"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.com" vc6-dll.dsp /MAKE ALL /REBUILD
-::premake --file vc6.pmk --target vs6
-premake --file vc6-msys2.pmk --target vs6
-if %errorlevel% neq 0 ( exit /b )
-"C:\Program Files (x86)\Microsoft Visual Studio\Common\MSDev98\Bin\msdev.com" vc6-run.dsp /MAKE ALL /REBUILD
-if %errorlevel% neq 0 ( exit /b )
+premake --file vc6.pmk --target vs6
+msdev.com vc6.dsp /MAKE ALL /REBUILD
+premake --file vc6-run.pmk --target vs6
+msdev.com vc6-run.dsp /MAKE ALL /REBUILD
 
 ::C:\dm\bin\implib /system vc6-run-dm32.lib vc6-run.dll
 ::if %errorlevel% neq 0 ( exit /b )
@@ -23,7 +17,7 @@ if %errorlevel% neq 0 ( exit /b )
 ::exit /b
 setlocal
 ::set PATH=E:\opt\svn\vc6\svn-win32-1.8.17-ap24\svn-win32-1.8.17\bin;%PATH%
-set PATH=e:\opt\opt.m32\usr\bin;%PATH%
+::set PATH=e:\opt\opt.m32\usr\bin;%PATH%
 vc6-run.exe https://github.com/cppcommons/d-sample/trunk
 endlocal
 
