cb733419a31902e1dd79916ad3f0c9371fac17c9
root <root@super-computer>
2017-11-21 16:04:08 +0900

    @

--- 005-dllcall/we/run32w-mk.bat
+++ 005-dllcall/we/run32w-mk.bat
@@ -1,5 +1,5 @@
-cl -MD -GX run32w.cpp /link /out:run32w.exe /subsystem:windows shell32.lib user32.lib
+cl -MD -GX run32w.cpp /link /out:run.exe /subsystem:windows shell32.lib user32.lib
 if %errorlevel% neq 0 ( exit /b )
-run32w.exe -nc a b c > ___tmp.txt
+run.exe -nc a b c > ___tmp.txt
 cat ___tmp.txt
-run32w.exe a b c
+run.exe a b c
