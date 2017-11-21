3fc108e22b6ddafcc1947f89c95752d185a765e2
root <root@super-computer>
2017-11-21 10:33:44 +0900

    @

--- 005-dllcall/we/wmain.cpp
+++ 005-dllcall/we/wmain.cpp
@@ -5,13 +5,21 @@
 #define ATTACH_PARENT_PROCESS (DWORD) - 1
 #endif
 
-typedef BOOL(WINAPI *proc_AttachConsole)(DWORD dwProcessId);
+bool AttachParentConsole()
+{
+	HMODULE hmod = LoadLibraryA("kernel32.dll");
+	if (!hmod)
+		return false;
+	typedef BOOL(WINAPI * proc_AttachConsole)(DWORD dwProcessId);
+	proc_AttachConsole addr_AttachConsole = (proc_AttachConsole)GetProcAddress(hmod, "AttachConsole");
+	if (!addr_AttachConsole)
+		return false;
+	return (bool)addr_AttachConsole(ATTACH_PARENT_PROCESS);
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	HMODULE hmod = LoadLibraryA("kernel32.dll");
-	proc_AttachConsole fn_AttachConsole = (proc_AttachConsole)GetProcAddress(hmod, "AttachConsole");
-	if (!(fn_AttachConsole && fn_AttachConsole(ATTACH_PARENT_PROCESS)))
+	if (!AttachParentConsole())
 		AllocConsole();
 	freopen("CONIN$", "r", stdin);
 	freopen("CONOUT$", "w", stdout);
@@ -20,7 +28,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	char a[1024];
 	memset(a, 0, 1024);
 	gets(a);
-	printf("a=%s", a);
+	printf("a=%s\n", a);
 	//MessageBox(NULL, "Hello Windows!", "MyFirst", MB_OK);
 	return 0;
 }
