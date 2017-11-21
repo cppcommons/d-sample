012b65a379c3663c21f3099d93b5c6bffd6ae322
root <root@super-computer>
2017-11-21 22:20:13 +0900

    @

--- 005-dllcall/we/np.bat
+++ 005-dllcall/we/np.bat
@@ -1,5 +1,6 @@
 edub np.dll build np.d --build=release def=WindowsVista c:\dm\lib\stlp45dm_static.lib wininet.lib 
 if %errorlevel% neq 0 ( exit /b )
+run32 np.dll
 ::start cmd /c run32 np.dll,runClient a b c
 start run32 -ac -w 2000 np.dll,runClient a b c
 ::run32 np.dll,runServer a b c
--- 005-dllcall/we/np.d
+++ 005-dllcall/we/np.d
@@ -115,7 +115,7 @@ extern (C) export int runClient(int argc, wchar** argv, DWORD with_console) //ex
 
 extern (C) export int RunMain(int argc, wchar** argv, DWORD with_console)
 {
-	import std.stdio : writeln;
+	import std.stdio; // : writeln;
 
 	writeln(`argc=`, argc);
 	for (int i = 0; i < argc; i++)
@@ -125,6 +125,9 @@ extern (C) export int RunMain(int argc, wchar** argv, DWORD with_console)
 	writeln(`with_console=`, with_console);
 	writeln(`with_console & 2=`, with_console & 2);
 	writeln("RunMain(DLang)");
+	write(`input: `);
+	auto line = stdin.readln();
+	writeln(line);
 	return 0;
 }
 
