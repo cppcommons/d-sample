04e1c840ccf48ae384eb787bda451c052d0fb49c
root <root@super-computer>
2017-11-22 00:23:23 +0900

    @

---
+++ 005-dllcall/sb/dl.bat
@@ -0,0 +1,3 @@
+svn co https://svn.apache.org/repos/asf/apr/apr/tags/1.6.2 src/apr
+svn co https://svn.apache.org/repos/asf/apr/apr-util/tags/1.6.0 src/apr-util
+svn co https://svn.apache.org/repos/asf/apr/apr-iconv/tags/1.2.1 src/apr-iconv
