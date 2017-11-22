b5da1da66d3fde0336825813f8548da319336cb5
root <root@super-computer>
2017-11-23 01:09:00 +0900

    @

--- 005-dllcall/we/run32w-mk.bat
+++ 005-dllcall/we/run32w-mk.bat
@@ -1,13 +1,19 @@
 del run32.exe
-::cl -MD -GX run32w.cpp /link /out:run.exe /subsystem:windows shell32.lib user32.lib
 ::cl -MT -GX run32w.cpp /link /out:run32-vc6.exe /subsystem:windows shell32.lib user32.lib
 ::if %errorlevel% neq 0 ( exit /b )
 ::cl -MT -GX -DCONSOLE_VERSION run32w.cpp /link /out:cmd32-vc6.exe /subsystem:console shell32.lib user32.lib
 ::if %errorlevel% neq 0 ( exit /b )
-g++ -o run32.exe run32w.cpp -static -mwindows
+
+cl -MT -EHsc run32w.cpp /link /out:run32.exe /subsystem:windows shell32.lib user32.lib
 if %errorlevel% neq 0 ( exit /b )
-g++ -o cmd32.exe -DCONSOLE_VERSION run32w.cpp -static
+cl -MT -EHsc -DCONSOLE_VERSION run32w.cpp /link /out:cmd32.exe /subsystem:console shell32.lib user32.lib
 if %errorlevel% neq 0 ( exit /b )
+
+g++ -o run32g.exe run32w.cpp -static -mwindows
+if %errorlevel% neq 0 ( exit /b )
+g++ -o cmd32g.exe -DCONSOLE_VERSION run32w.cpp -static
+if %errorlevel% neq 0 ( exit /b )
+
 run32.exe -nc np bb cc > ___tmp.txt
 cat ___tmp.txt
 run32.exe np@:RunMain bb cc
