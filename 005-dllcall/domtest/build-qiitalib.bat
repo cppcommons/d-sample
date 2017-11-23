378cc6ebbba9c187a52bb00cc05f88812a313ee8
root <root@super-computer>
2017-11-23 18:12:05 +0900

    @

---
+++ 005-dllcall/domtest/build-qiitalib.bat
@@ -0,0 +1,6 @@
+edub qiitalib.lib build ^
+qiitalib.d ^
+characterencodings.d@https://github.com/adamdruppe/arsd/blob/master/characterencodings.d ^
+https://github.com/adamdruppe/arsd/blob/master/dom.d ^
+[vibe-d] [dateparser] "[d2sqlite3:  :without-lib]"
+if %errorlevel% neq 0 (exit /b)
