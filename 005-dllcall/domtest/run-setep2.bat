b424c2151aab7ec222eb4593b8d9a47b95243a6b
root <root@super-computer>
2017-11-24 18:06:59 +0900

    @

--- 005-dllcall/domtest/run-setep2.bat
+++ 005-dllcall/domtest/run-setep2.bat
@@ -1,4 +1,6 @@
 chcp 65001
 
-edub step2-ms64.exe run arch=ms64 step2.d [vibe-d] [dateparser]
+edub step2-ms64.exe run arch=ms64 step2.d ^
+sqlite-win-64bit-3210000-ms64.lib qiitadb-ms64.lib ^
+[vibe-d] [dateparser] "[d2sqlite3::without-lib]"
 if %errorlevel% neq 0 (exit /b)
--- 005-dllcall/domtest/step2.d
+++ 005-dllcall/domtest/step2.d
@@ -1,3 +1,6 @@
+import qiitadb;
+import d2sqlite3, std.typecons : Nullable;
+
 import dateparser;
 import vibe.data.json, vibe.data.serialization;
 import core.time;
@@ -24,86 +27,24 @@ private void exit(int code)
 
 int main(string[] args)
 {
-	//File f = File("___g_total.txt", "r");
-	writeln("Reading JSON...");
-	string json = cast(string) read("___g_total.txt");
-	SysTime v_start;
-	writeln("Reading JSON...done!");
-
-	import std.json;
-	v_start = Clock.currTime();
-	write("Parsing JSON(1)...");
-	stdout.flush();
-	JSONValue j = parseJSON(json);
-	writeln(Clock.currTime() - v_start);
-
-	v_start = Clock.currTime();
-	write("Parsing JSON...");
-	stdout.flush();
-	Json[] records = parseJsonString(json).get!(Json[]);
-	writeln(Clock.currTime() - v_start);
-
-	writeln(records.length);
-
-	v_start = Clock.currTime();
-	write("Sorting Records...");
-	stdout.flush();
-	Json*[] reverse_array;
-	long liked_count = 0;
-	foreach (ref rec; records)
-	{
-		if (rec[`likes_count`].get!long < 50)
-			continue;
-		reverse_array ~= &rec;
-		/+
-		if (rec[`likes_count`].get!long >= 50)
-		{
-			liked_count++;
-		}
-		+/
-	}
-	//reverse(reverse_array);
-	bool myComp(Json* x, Json* y)
-	{
-		if ((*x)[`likes_count`].get!long != (*y)[`likes_count`].get!long)
-		{
-			return (*x)[`likes_count`].get!long > (*y)[`likes_count`].get!long;
-		}
-		return (*x)[`created_at`].get!string > (*y)[`created_at`].get!string;
-	}
-
-	sort!myComp(reverse_array);
-	writeln(Clock.currTime() - v_start);
-	for (int i = 0; i < min(reverse_array.length, 20); i++)
-	{
-		Json* rec = reverse_array[i];
-		writeln((*rec).serializeToJsonString);
-	}
-	writeln(reverse_array.length);
-
-	Json jsonObj = Json.emptyArray;
-	foreach (rec; reverse_array)
-	{
-		jsonObj.appendArrayElement(*rec);
-	}
-
-	assert(jsonObj.type == Json.Type.Array);
-	Json[] elems = jsonObj.get!(Json[]);
+	Database db1 = ql_get_db_1(`___db1.db3`);
+	long count1 = 0;
 	File f = File("___j_like_over_50.txt", "w");
 	f.write("[");
 	long count = 0;
-	foreach (ref elem; elems)
+	ResultRange results = db1.execute(
+			"SELECT json FROM qiita WHERE likes_count >= 50 ORDER BY likes_count desc");
+	foreach (Row row; results)
 	{
 		if (count > 0)
 			f.write(",\n ");
 		count++;
-		f.write(elem.serializeToJsonString);
+		auto json = row["json"].as!string;
+		f.write(json);
 	}
 	f.write("]");
 	f.write("\n");
 	f.close();
-
 	exit(0);
-
 	return 0;
 }
