e62af513aea9d0667feddd95ab9e89cd2e78797a
root <root@super-computer>
2017-11-26 17:38:54 +0900

    @

--- 005-dllcall/domtest/run-setep3.bat
+++ 005-dllcall/domtest/run-setep3.bat
@@ -1,6 +1,8 @@
 chcp 65001
 
 edub step3.exe run arch=dm32 step3.d ^
-sqlite-win-32bit-3200100-dm32.lib qiitalib.lib qiitadb.lib ^
-[vibe-d] [dateparser] "[d2sqlite3::without-lib]"
+characterencodings.d@https://github.com/adamdruppe/arsd/blob/master/characterencodings.d ^
+https://github.com/adamdruppe/arsd/blob/master/dom.d ^
+qiitadb.lib qiitalib.lib sqlite-win-32bit-3200100-dm32.lib ^
+[vibe-d] [dateparser] "[my-hibernated:@hibernated-0.3.2]" "[d2sqlite3::without-lib]"
 if %errorlevel% neq 0 (exit /b)
--- 005-dllcall/domtest/step3.d
+++ 005-dllcall/domtest/step3.d
@@ -1,6 +1,11 @@
 import qiitalib;
 import qiitadb;
+
 import d2sqlite3, std.typecons : Nullable;
+
+import ddbc;
+import hibernated.core;
+import std.algorithm;
 
 import dateparser;
 import vibe.data.json, vibe.data.serialization;
@@ -27,28 +32,48 @@ private void exit(int code)
 	std.c.stdlib.exit(code);
 }
 
-__gshared static Database db1;
+__gshared static
+{
+	Session g_Session;
+	Connection g_Conn;
+	ddbc.Statement g_Statement;
+
+}
+
+shared static ~this()
+{
+	g_Statement.close();
+	g_Session.close();
+}
+
 shared static this()
 {
-	/*Database*/
-	db1 = ql_get_db_1(`___db1.db3`);
+	g_Session = g_SessionFactory.openSession();
+	g_Conn = (cast(SessionImpl) g_Session).conn;
+	g_Statement = g_Conn.createStatement();
 }
 
 int main(string[] args)
 {
-	//sleepForWeeks(10);
-	//sleepUntil(DateTime(2020, 1, 1, 20, 15, 32));
-	//exit(0);
+	Query q = g_Session.createQuery(
+			"FROM Qiita WHERE likes_count >= 50 ORDER BY likes_count desc, created_at desc");
+	Qiita[] rows = q.list!Qiita();
+	Json[] records;
+	foreach (row; rows)
+	{
+		string json = row.json;
+		records ~= parseJsonString(json);
+	}
+	/+
 	ResultRange results = db1.execute(
 			`SELECT json FROM qiita WHERE likes_count >= 50 ORDER BY likes_count desc, created_at desc`);
-	//if (results.empty)
-	//	return false;
 	Json[] records;
 	foreach (Row row; results)
 	{
 		string json = row["json"].as!string;
 		records ~= parseJsonString(json);
 	}
+	+/
 
 	//long max_count = 5000;
 	//long max_count = 1000;
@@ -147,7 +172,7 @@ int main(string[] args)
 				tags_html,
 				//std.xml.encode((*rec)[`body`].get!string).replace("\n", `<br />`) //
 				(*rec)[`rendered_body`].get!string //
-		));
+				));
 		f.writeln("<br />");
 	}
 	f.writeln("	</body>");
@@ -159,6 +184,16 @@ int main(string[] args)
 
 void create_user_page(string target_user_id, long user_permanent_id /+, ref Json[] records0+/ )
 {
+	Query q = g_Session.createQuery("FROM Qiita WHERE user_id=:Id ORDER BY likes_count desc, created_at desc")
+		.setParameter("Id", target_user_id);
+	Qiita[] rows = q.list!Qiita();
+	Json[] records;
+	foreach (row; rows)
+	{
+		string json = row.json;
+		records ~= parseJsonString(json);
+	}
+	/+
 	ResultRange results = db1.execute(
 			format!`SELECT json FROM qiita WHERE user_id="%s" ORDER BY likes_count desc, created_at desc`(
 			target_user_id));
@@ -170,6 +205,7 @@ void create_user_page(string target_user_id, long user_permanent_id /+, ref Json
 		string json = row["json"].as!string;
 		records ~= parseJsonString(json);
 	}
+	+/
 	string file_name = format!"qranking.github.io/user/%d.html"(user_permanent_id);
 	if (exists(file_name))
 		return;
@@ -185,13 +221,15 @@ void create_user_page(string target_user_id, long user_permanent_id /+, ref Json
 	f.writeln(format!`<h1>Qiitaいいね数ランキング (%s さんの投稿分)</h1>`(
 			target_user_id));
 	f.writeln(`</div><!--class="headerContainer"-->`);
-	f.writeln(`<p><a href="#" onclick="javascript:window.history.back(-1);return false;">[戻る]</a></p>`);
+	f.writeln(
+			`<p><a href="#" onclick="javascript:window.history.back(-1);return false;">[戻る]</a></p>`);
 	f.writeln(`<p><i><img width="16" height="16" src="../thumb-up-120px.png" /></i>が同じ値の場合は投稿日時の新しいものが上位としています。</p>`);
 	f.writeln(`<p><i><img width="16" height="16" src="../thumb-up-120px.png" /></i>がついていない記事は表示していません。</p>`);
 	for (int i = 0; i < min(100, records.length); i++)
 	{
 		Json* rec = &records[i];
-		if ((*rec)[`likes_count`].get!long == 0) break;
+		if ((*rec)[`likes_count`].get!long == 0)
+			break;
 		//writeln((*rec).serializeToJsonString);
 		//writeln((*rec)[`title`].get!string);
 		//writeln((*rec)[`likes_count`].get!long);
@@ -222,7 +260,7 @@ void create_user_page(string target_user_id, long user_permanent_id /+, ref Json
 		f.writeln(format!`<table border="1">
 <tr>
 	<td rowspan="3"><center>%sさんの<br />%d位</center></td>
-	<td colspan="3">
+	<td colspan="4">
 		<kbd><i><img alt="いいね" width="16" height="16" src="../thumb-up-120px.png" /></i>%d</kbd>
 		<a target="_blank" href="%s">%s</a>
 	</td>
@@ -261,7 +299,7 @@ void create_user_page(string target_user_id, long user_permanent_id /+, ref Json
 				(*rec)[`user`][`profile_image_url`].get!string, //
 				tags_html,
 				(*rec)[`rendered_body`].get!string //
-		));
+				));
 		f.writeln("<br />");
 	}
 	f.writeln("	</body>");
