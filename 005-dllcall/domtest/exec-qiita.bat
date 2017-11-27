6db7686da3739fbe5572fab229ac43fcf4117da1
root <root@super-computer>
2017-11-27 16:19:40 +0900

    @

--- 005-dllcall/domtest/exec-qiita.bat
+++ 005-dllcall/domtest/exec-qiita.bat
@@ -4,5 +4,5 @@ qiita.d ^
 characterencodings.d@https://github.com/adamdruppe/arsd/blob/master/characterencodings.d ^
 https://github.com/adamdruppe/arsd/blob/master/dom.d ^
 qiitadb.lib qiitalib.lib sqlite-win-32bit-3200100-dm32.lib ^
-[vibe-d] [dateparser] "[my-hibernated:@hibernated-0.3.2]" "[d2sqlite3::without-lib]"
+[vibe-d{:}data] [dateparser] "[my-hibernated:@hibernated-0.3.2]" "[d2sqlite3::without-lib]"
 if %errorlevel% neq 0 (exit /b)
